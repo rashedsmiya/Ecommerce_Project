fdcf2029ea42cae64cd46de0c0df2d2503559fb6
rashedsmiya <rashedmiyaofficial@gmail.com>
2024-12-13 17:35:47 +0600

    add all file

--- wp-content/themes/learnwp/pages/custompage.php
+++
@@ -1,5 +0,0 @@
-<article>
-      <h1>London</h1>
-      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Officia iure doloribus sapiente, officiis quibusdam atque a laudantium eum accusamus, quis magni culpa quisquam unde ab, ipsa delectus placeat eligendi. Placeat.</p>
-      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nulla dolore quas adipisci quisquam provident quae, corporis nemo deleniti labore dolorum omnis saepe accusantium ab minima voluptatem beatae inventore recusandae incidunt.</p>
-</article>
--- wp-content/themes/learnwp/template-parts/content-video.php
+++
@@ -1,6 +0,0 @@
-<article>
-    <h1>This is video Post</h1>
-    <h1> <?php the_title();?> </h1>
-    <?php the_time(); ?>
-    <a href="<?php //echo get_permalink(); ?>"> <?php _e("Read Post", 'learnwp');?></a>
-</article> 
