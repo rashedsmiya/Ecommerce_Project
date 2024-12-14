2e09b60bcf35751b03032aa3aa232e5f66296b19
rashedsmiya <rashedmiyaofficial@gmail.com>
2024-12-14 23:25:20 +0600

    add

--- crud.php/index.php
+++ crud.php/index.php
@@ -1,10 +1,3 @@
 <?php 
 
-    $conn = mysqli_connect("localhost", "root", "", "cmbd245");
-    if(!$conn){
-        echo "Database connection failed";
-    } else {
-        echo "Database connection successfull";
-    }
-
-    
+     
