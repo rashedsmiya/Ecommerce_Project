641e907355b6696fb268e1e75df3773fe1a61ffa
rashedsmiya <rashedmiyaofficial@gmail.com>
2024-12-15 14:58:46 +0600

    add all file

--- crud.php/index.php
+++ crud.php/index.php
@@ -1,3 +1,11 @@
 <?php 
-
      
+     try {
+        $conn = mysqli_connect("localhost", "root", "", "cmbd257");
+    
+         echo "Connected susccessfully";
+         
+    } catch (mysqli_sql_exception $e) {
+        
+        echo "Connection Failed: " . $e->getMessage();
+    }
