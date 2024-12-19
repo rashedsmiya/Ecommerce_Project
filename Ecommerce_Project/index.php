4ebf563d72723c960c2d82621fb728563708ba75
rashedsmiya <rashedmiyaofficial@gmail.com>
2024-12-19 18:12:13 +0600

    add all

---
+++ Ecommerce_Project/index.php
@@ -0,0 +1,9 @@
+<?php 
+  require_once './header.php';
+  require_once './components/homeSlider.php';
+?>
+ 
+
+<?php 
+  require_once './footer.php';
+?>
---
+++ Ecommerce_Project/navbar.php
@@ -0,0 +1,32 @@
+<nav class="navbar navbar-expand-lg bg-dark navbar-dark">
+  <div class="container">
+    <a class="navbar-brand" href="#">Cleantowers</a>
+    <button class="d-block d-lg-none ms-auto btn text-white" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasExample" aria-controls="offcanvasExample">
+      <i class="fa-solid fa-cart-shopping"></i>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="./shop.php">Shop</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">About</a>
+        </li>
+      </ul>  
+       <div class="" style="width: 280px">
+      <form class="d-flex" role="search">
+        <input class="form-control" type="search" placeholder="Search Product..." aria-label="Search">
+        <button class="btn btn-success" type="submit">
+          <i class="fa-solid fa-magnifying-glass"></i> 
+        </button> 
+      </form> 
+    </div> 
+  </div>
+    <button class="d-none d-md-block ms-2 btn text-white" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasExample" aria-controls="offcanvasExample">
+      <i class="fa-solid fa-cart-shopping"></i>
+    </button> 
+  </div> 
+</nav> 
