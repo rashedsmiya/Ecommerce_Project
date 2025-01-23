63a7202fb3767b9bada7e00d717f1397b00b2002
rashedsmiya <rashedmiyaofficial@gmail.com>
2025-01-23 15:33:10 +0600

    customize_theme_color

--- Ecommerce_Project/sign-up.php
+++ Ecommerce_Project/sign-up.php
@@ -1,20 +1,28 @@
 <?php 
+
 require_once './header.php';
+if(isset($_POST['signup123'])){
+
+}
+
 ?>
 
 <div class="container">
     <div class="row min-vh-100 d-flex">
         <div class="col-md-4 border rounded shadow p-3 m-auto">
             <h3 class="mb-3">Sign Up</h3>
-            <form action="mb-4" method="post">
-                <div class="mb-3">
+            <form action="" method="post">
+                <div class="row ">
+                   <div class="col-6">
                     <input type="text" placeholder="First Name" name="fname" class="form-control">
                 </div>
-                <div class="mb-3">
-                    <input type="email" placeholder="Email" name="email" class="form-control">
-                </div>
-                <div class="mb-3">
-                    <input type="password" placeholder="Passwoerd" name="password" class="form-control">
+                <div class="row mb-3">
+                    <div class="col-6">
+                        <input type="email" placeholder="Email" name="email" class="form-control">
+                    </div> 
+                    <div class="mb-6">
+                        <input type="password" placeholder="Confirm Passwoerd" name="password" class="form-control">
+                    </div>
                 </div>
             </form>
         </div>
